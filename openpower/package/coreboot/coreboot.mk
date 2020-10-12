6211aa625d75ae810f7fce17d9876dc641c1c403
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-10-12 23:22:42 +0200

    openpower/package/coreboot: download submodules
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- openpower/package/coreboot/coreboot.mk
+++ openpower/package/coreboot/coreboot.mk
@@ -6,6 +6,7 @@
 COREBOOT_VERSION = $(call qstrip,$(BR2_COREBOOT_VERSION))
 COREBOOT_SITE = https://github.com/3mdeb/coreboot.git
 COREBOOT_SITE_METHOD = git
+COREBOOT_GIT_SUBMODULES = YES
 
 COREBOOT_LICENSE = GPLv2
 COREBOOT_LICENSE_FILES = LICENSE
@@ -17,7 +18,7 @@ COREBOOT_INSTALL_TARGET = NO
 COREBOOT_ENV_VARS=$(TARGET_MAKE_ENV)
 
 define COREBOOT_BUILD_CMDS
-        $(COREBOOT_ENV_VARS) bash -c 'cd $(@D) && cp configs/$(call qstrip,$(BR2_COREBOOT_CONFIG_FILE)) .config && $(MAKE) olddefconfig && $(MAKE)'
+        $(COREBOOT_ENV_VARS) bash -c 'cd $(@D) && $(MAKE) crossgcc-ppc64 && cp configs/$(call qstrip,$(BR2_COREBOOT_CONFIG_FILE)) .config && $(MAKE) olddefconfig && $(MAKE)'
 endef
 
 define COREBOOT_INSTALL_IMAGES_CMDS
