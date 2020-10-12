9c2ac8d82e367a8f22fe89e10677be99f43c8bb5
Michał Żygowski <michal.zygowski@3mdeb.com>
2020-10-13 00:25:18 +0200

    openpower/package/coreboot/coreboot.mk: fix OpenSSL missing in PKG_CONFIG
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>

--- openpower/package/coreboot/coreboot.mk
+++ openpower/package/coreboot/coreboot.mk
@@ -10,20 +10,26 @@ COREBOOT_GIT_SUBMODULES = YES
 
 COREBOOT_LICENSE = GPLv2
 COREBOOT_LICENSE_FILES = LICENSE
-COREBOOT_DEPENDENCIES = host-binutils
+COREBOOT_DEPENDENCIES = host-binutils host-libopenssl
 
 COREBOOT_INSTALL_IMAGES = YES
 COREBOOT_INSTALL_TARGET = NO
 
-COREBOOT_ENV_VARS=$(TARGET_MAKE_ENV)
+COREBOOT_ENV_VARS = $(TARGET_MAKE_ENV)
+COREBOOT_ENV_VARS += \
+	PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
+	PKG_CONFIG_SYSROOT_DIR="/" \
+	PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 \
+	PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 \
+	PKG_CONFIG_LIBDIR="$(HOST_DIR)/lib/pkgconfig:$(HOST_DIR)/share/pkgconfig"
 
 define COREBOOT_BUILD_CMDS
-        $(COREBOOT_ENV_VARS) bash -c 'cd $(@D) && $(MAKE) crossgcc-ppc64 CPUS=8 && cp configs/$(call qstrip,$(BR2_COREBOOT_CONFIG_FILE)) .config && $(MAKE) olddefconfig && $(MAKE)'
+    $(COREBOOT_ENV_VARS) bash -c 'cd $(@D) && $(MAKE) crossgcc-ppc64 CPUS=8 && cp configs/$(call qstrip,$(BR2_COREBOOT_CONFIG_FILE)) .config && $(MAKE) olddefconfig && $(MAKE)'
 endef
 
 define COREBOOT_INSTALL_IMAGES_CMDS
-        cd $(@D) && cp build/coreboot.signed  $(STAGING_DIR)/coreboot_build_images/ && \
-                cp build/bootblock.signed.ecc  $(STAGING_DIR)/coreboot_build_images/
+    cd $(@D) && cp build/coreboot.signed  $(STAGING_DIR)/coreboot_build_images/ && \
+        cp build/bootblock.signed.ecc  $(STAGING_DIR)/coreboot_build_images/
 endef
 
 $(eval $(generic-package))
